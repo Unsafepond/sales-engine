f1feb9868590950770110478dd88e93d0eba9910
Michael Dorrance <dorrancemd@gmail.com>
2015-06-17 15:18:16 -0600

    merchant_repo tests through find_by_x

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -8,13 +8,34 @@ require_relative 'sales_engine'
 
 
 class MerchantRepository
-  attr_reader :merchants
+  attr_reader :merchants,
+              :all
   def initialize(hashes)
     @all = hashes.map { |hash| Merchant.new(hash.to_hash, self)}
   end
 
   def find_all_by_name(name)
-    @all.select { |merchant| merchant.name == name}
+    @all.select { |merchant| merchant.name == name }
+  end
+
+  def random
+    @all.shuffle
+  end
+
+  def find_by_id(id)
+    @all.select { |merchant| merchant.id == id }
+  end
+
+  def find_by_name(name)
+    @all.select { |merchant| merchant.name == name }
+  end
+
+  def find_by_created_at(create_date)
+    @all.select { |merchant| merchant.created_at == create_date }
+  end
+
+  def find_by_updated_at(update_date)
+    @all.select { |merchant| merchant.updated_at == update_date }
   end
 
 
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -8,6 +8,80 @@ class MerchantRepositoryTest < Minitest::Test
     assert MerchantRepository
   end
 
+  def test_find_all_merchants
+    data_directory = File.expand_path 'fixtures', __dir__
+    se = SalesEngine.new(data_directory)
+    se.startup
+    repo = se.merchant_repository
+    assert_equal 5, repo.all.count
+
+  end
+
+  def test_find_random_merchants
+    data_directory = File.expand_path 'fixtures', __dir__
+    se = SalesEngine.new(data_directory)
+    se.startup
+    repo = se.merchant_repository
+    merchant_permutations = repo.all.permutation.to_a
+      assert merchant_permutations.include?(repo.random)
+
+  end
+
+  def test_find_merchant_by_id
+    data_directory = File.expand_path 'fixtures', __dir__
+    se = SalesEngine.new(data_directory)
+    se.startup
+    repo = se.merchant_repository
+
+    ids = repo.find_by_id("2")
+    ids.each do |id|
+      assert_equal 2, id.id
+    end
+  end
+  def test_find_merchant_by_name
+    data_directory = File.expand_path 'fixtures', __dir__
+    se = SalesEngine.new(data_directory)
+    se.startup
+    repo = se.merchant_repository
+
+    names = repo.find_by_name("Schroeder-Jerde")
+    names.each do |name|
+      assert_equal "Schroeder-Jerde", repo.find_by_name("Schroeder-Jerde")
+    end
+  end
+  def test_find_merchant_by_created_at
+    data_directory = File.expand_path 'fixtures', __dir__
+    se = SalesEngine.new(data_directory)
+    se.startup
+    repo = se.merchant_repository
+
+    created_at = repo.find_by_created_at("2012-03-27 14:53:59 UTC")
+    created_at.each do |create|
+      assert_equal "2012-03-27 14:53:59 UTC", create.created_at
+    end
+      created_at = repo.find_by_created_at("2012-03-2 14:53:59 UTC")
+      created_at.each do |create|
+        assert_equal "", create.created_at
+    end
+  end
+  def test_find_merchant_by_updated_at
+    data_directory = File.expand_path 'fixtures', __dir__
+    se = SalesEngine.new(data_directory)
+    se.startup
+    repo = se.merchant_repository
+
+    updated_at = repo.find_by_updated_at("2012-03-27 14:53:59 UTC")
+    updated_at.each do |update|
+      assert_equal "2012-03-27 14:53:59 UTC", update.updated_at
+    end
+
+    updated_at = repo.find_by_updated_at("2012-27 14:53:59 UTC")
+    updated_at.each do |update|
+      assert_equal "", update.updated_at
+    end
+  end
+
+
 
 
 end
