4e339b6d293e3b3807bb7b91177620f067cda849
Michael Dorrance <dorrancemd@gmail.com>
2015-06-17 19:30:47 -0600

    adds remaining repos to sales_engine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -13,17 +13,25 @@ class SalesEngine
   end
 
   attr_accessor :merchant_repository,
-                :invoice_repository
+                :invoice_repository,
+                :item_repository,
+                :invoice_repository,
+                :customer_repository,
+                :transactions
 
   def startup
     hashes = CSV.read "#{@data_dir}/merchants.csv", headers: true, header_converters: :symbol
     hashes2 = CSV.read "#{@data_dir}/invoices.csv", headers: true, header_converters: :symbol
+    # hashes3 = CSV.read "#{@data_dir}/items.csv", headers: true, header_converters: :symbol
     hashes4 = CSV.read "#{@data_dir}/invoice_items.csv", headers: true, header_converters: :symbol
+    # hashes5 = CSV.read "#{@data_dir}/customers.csv", headers: true, header_converters: :symbol
+    # hashes6 = CSV.read "#{@data_dir}/transactions.csv", headers: true, header_converters: :symbol
     @merchant_repository      = MerchantRepository.new(hashes)
     @invoice_repository      = InvoiceRepository.new(hashes2)
-
+    # @items_repository      = ItemsRepository.new(hashes3)
     @invoice_item_repository = InvoiceItemRepository.new(hashes4)
-
+    # @customer_repository = CustomerRepository.new(hashes5)
+    # @transaction_repository = TransactionRepository.new(hashes6)
   end
 end
 
